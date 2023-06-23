d83959e276ff62372e1434da033e3c6745ca20f7
Cholwe Muleya <cholwemuleya@Cholwes-MacBook-Pro.local>
2023-06-23 12:20:34 +0200

    Home page complete

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var typed = new Typed("#typed", {
+    strings:["a Front-end Developer" , "a Back-end Developer" , "an Ethical Hacker"], 
+    typeSpeed: 40,
+    backSpeed: 40,
+    backDelay: 2000,
+    loop: true
+});
